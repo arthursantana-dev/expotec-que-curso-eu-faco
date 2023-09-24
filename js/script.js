4459868ef50ac8d7b667b789e59e713830601f71
Arthur Santana <arthur.alexandre330@gmail.com>
2023-09-24 11:49:42 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,114 @@
+const data = [
+	{
+		question: 'Gosto de Matemática',
+		radioButtonType: 'muito/pouco',
+		ADMPoints: 1,
+		DSPoints: 2,
+		MECAPoints: 3,
+		EDFPoints: 1
+	},
+	{
+		question: 'Lido bem com Pessoas',
+		radioButtonType: 'sim/nao',
+		ADMPoints: 3,
+		DSPoints: 1,
+		MECAPoints: 1,
+		EDFPoints: 2
+	},
+	{
+		question: 'Gosto de Matemática',
+		radioButtonType: 'muito/pouco',
+		ADMPoints: 1,
+		DSPoints: 2,
+		MECAPoints: 3,
+		EDFPoints: 1
+	},
+	{
+		question: 'Lido bem com Pessoas',
+		radioButtonType: 'sim/nao',
+		ADMPoints: 3,
+		DSPoints: 1,
+		MECAPoints: 1,
+		EDFPoints: 2
+	}
+
+]
+
+const main = document.querySelector('main')
+let radioButtonToAnswerId = 0
+
+
+data.map((q, i) => {
+	let maxName = ""
+	let minName = ""
+
+	if (q.radioButtonType == 'sim/nao') {
+		maxName = 'Sim'
+		minName = 'Não'
+	} else if (q.radioButtonType == 'muito/pouco') {
+		maxName = 'Muito'
+		minName = 'Pouco'
+	}
+
+	main.innerHTML += `<div class="question disabled" data-key=${i}>
+	<div class="question__index">
+		<p class="question__index__value">Questão ${i + 1}</p>
+	</div>
+	<h1 class="question__question">${q.question}</h1>
+	<div class="question__radio-wrapper">
+		<div class="radio-wrapper">
+			<div class="input-radio-wrapper input-radio-wrapper--max">
+				<input class="radio-max radio-big" type="radio" name="${i}" value="0" />
+			</div>
+			<p class="radio-wrapper-desc radio-wrapper-desc--max">${maxName}!</p>
+		</div>
+		<div class="input-radio-wrapper input-radio-wrapper--max-mid">
+			<input class="radio-max-mid radio-medium" type="radio" name="${i}" value="1" />
+		</div>
+		<div class="radio-wrapper">
+			<div class="input-radio-wrapper"><input class="radio-mid" type="radio" name="${i}" value="2" /></div>
+			<p class="radio-wrapper-desc radio-wrapper-desc--mid">meh...</p>
+		</div>
+		<div class="input-radio-wrapper input-radio-wrapper--min-mid">
+			<input class="radio-min-mid radio-medium" type="radio" name="${i}" value="3" />
+		</div>
+		<div class="radio-wrapper">
+			<div class="input-radio-wrapper input-radio-wrapper--min"><input class="radio-min radio-big"
+					type="radio" name="${i}" value="4" />
+			</div>
+			<p class="radio-wrapper-desc radio-wrapper-desc--min">${minName}!</p>
+		</div>
+	</div>`
+})
+
+const radioButtons = document.querySelectorAll('input[type="radio"]')
+
+document.querySelector(`div[data-key="${radioButtonToAnswerId}"]`).classList.remove('disabled')
+
+function allowNextRadioButtonInnerFunction(r) {
+	r.addEventListener('click', () => {
+		if (radioButtonToAnswerId < data.length - 1) {
+			radioButtonToAnswerId++
+			document.querySelector(`div[data-key="${radioButtonToAnswerId}"]`).classList.remove('disabled')
+			console.log(radioButtonToAnswerId);
+			console.log(document.querySelector(`div[data-key="${radioButtonToAnswerId}"]`));
+			allowNextRadioButton()
+		} else {
+			document.querySelector('button')
+		}
+	})
+}
+
+function allowNextRadioButton() {
+	const currentGroup = document.querySelectorAll(`input[name="${radioButtonToAnswerId}"]`)
+
+	Array.from(currentGroup).map(r => allowNextRadioButtonInnerFunction(r))
+}
+
+allowNextRadioButton()
+
+
+function calculateCourse() {
+	const selectedRadioButtons = Array.from(radioButtons).filter(r => r.checked)
+
+}
