7ce73a78434b12c9f06208a93d6d91bf4e4e25b2
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2023-10-04 12:17:37 -0300

    correção bug (questão respondida em branco)

--- js/script.js
+++ js/script.js
@@ -1,37 +1,27 @@
-const data = [
-	{
-		question: 'Gosto de Matemática',
-		radioButtonType: 'muito/pouco',
-		ADMPoints: 1,
-		DSPoints: 2,
-		MECAPoints: 3,
-		EDFPoints: 1
-	},
-	{
-		question: 'Lido bem com Pessoas',
-		radioButtonType: 'sim/nao',
-		ADMPoints: 3,
-		DSPoints: 1,
-		MECAPoints: 1,
-		EDFPoints: 2
-	},
-	{
-		question: 'Sou criativo',
-		radioButtonType: 'sim/nao',
-		ADMPoints: 3,
-		DSPoints: 2,
-		MECAPoints: 1,
-		EDFPoints: 1
-	},
-	{
-		question: 'Gosto de desenhar',
-		radioButtonType: 'muito/pouco',
-		ADMPoints: 1,
-		DSPoints: 1,
-		MECAPoints: 3,
-		EDFPoints: 4
+class Question{
+	constructor(question, radioButtonType, ADMPoints, DSPoints, MECAPoints, EDFPoints){
+		this.question = question
+		this.radioButtonType = radioButtonType
+		this.ADMPoints = ADMPoints
+		this.DSPoints = DSPoints
+		this.MECAPoints = MECAPoints
+		this.EDFPoints = EDFPoints
 	}
+}
 
+const data = [
+	new Question('Gosto de Matemática, em especial de Geometria e Álgebra', 'muito/pouco', 5, 5, 5, 4),
+    new Question('Lido bem com Pessoas', 'muito/pouco', 5, 3, 3, 4),
+    new Question('Curto robôs', 'muito/pouco', 3, 5, 5, 3),
+    new Question('Me identifico com as Ciências Exatas', 'muito/pouco', 4, 5, 5, 4),
+    new Question('Gostava de brinquedos de construção na infância, como Lego', 'muito/pouco', 3, 4, 3, 5),
+    new Question('Tenho afinidade com as Ciências Humanas', 'sim/nao', 5, 3, 3, 5),
+    new Question('Tenho senso de liderança', 'sim/nao', 5, 3, 3, 4),
+    new Question('Prefiro trabalhar sozinho', 'sim/nao', 3, 5, 4, 3),
+    new Question('Gosto de ler/escrever histórias e textos', 'sim/nao', 5, 5, 3, 3),
+    new Question('Sou criativo(a)', 'muito/pouco', 5, 5, 3, 4),
+    new Question('Sou organizado(a)', 'muito/pouco', 5, 5, 3, 3),
+    new Question('Gosto de desenhar', 'muito/pouco', 3, 3, 4, 5),
 ]
 
 const main = document.querySelector('main')
@@ -60,25 +50,25 @@ data.map((q, i) => {
 	<div class="question__radio-wrapper">
 		<div class="radio-wrapper">
 			<div class="input-radio-wrapper input-radio-wrapper--max">
-				<input class="radio-max radio-big radio-positive" type="radio" name="${i}" value="4" />
+				<input class="radio-max radio-big radio-positive" type="radio" name="${i}" value="5" />
 			</div>
 			<p class="radio-wrapper-desc radio-wrapper-desc--max">${maxName}!</p>
 		</div>
 		<div class="input-radio-wrapper input-radio-wrapper--max-mid">
-			<input class="radio-max-mid radio-medium radio-positive" type="radio" name="${i}" value="3" />
+			<input class="radio-max-mid radio-medium radio-positive" type="radio" name="${i}" value="4" />
 		</div>
 		<div class="radio-wrapper">
 			<div class="input-radio-wrapper">
-				<input class="radio-mid" type="radio" name="${i}" value="2" />
+				<input class="radio-mid" type="radio" name="${i}" value="3" />
 			</div>
 			<p class="radio-wrapper-desc radio-wrapper-desc--mid">meh...</p>
 		</div>
 		<div class="input-radio-wrapper input-radio-wrapper--min-mid">
-			<input class="radio-min-mid radio-medium" type="radio" name="${i}" value="1" />
+			<input class="radio-min-mid radio-medium" type="radio" name="${i}" value="2" />
 		</div>
 		<div class="radio-wrapper">
 			<div class="input-radio-wrapper input-radio-wrapper--min"><input class="radio-min radio-big"
-					type="radio" name="${i}" value="0" />
+					type="radio" name="${i}" value="1" />
 			</div>
 			<p class="radio-wrapper-desc radio-wrapper-desc--min">${minName}!</p>
 		</div>
@@ -89,22 +79,36 @@ const radioButtons = document.querySelectorAll('input[type="radio"]')
 
 document.querySelector(`div[data-key="${radioButtonToAnswerId}"]`).classList.remove('disabled')
 
+let pressedRadiosName = []
+
 function allowNextRadioButtonInnerFunction(r) {
 	r.addEventListener('click', () => {
+
 		if (radioButtonToAnswerId < data.length - 1) {
-			radioButtonToAnswerId++
-			document.querySelector(`div[data-key="${radioButtonToAnswerId}"]`).classList.remove('disabled')
-			allowNextRadioButton()
+			
+			let groupWasPressed = pressedRadiosName.indexOf(r.name) != -1
+			console.log(`Btn name ${r.name}: ${groupWasPressed}`)
+
+			if(!groupWasPressed) {
+				pressedRadiosName.push(r.name) 
+				radioButtonToAnswerId++
+				document.querySelector(`div[data-key="${radioButtonToAnswerId}"]`).classList.remove('disabled')
+				allowNextRadioButton()
+			}
+
 		} else {
 			document.querySelector('button.submit-button').classList.remove('disabled')
 		}
 	})
+
 }
 
 function allowNextRadioButton() {
 	const currentGroup = document.querySelectorAll(`input[name="${radioButtonToAnswerId}"]`)
 
-	Array.from(currentGroup).map(r => allowNextRadioButtonInnerFunction(r))
+	Array.from(currentGroup).map(r => {
+		allowNextRadioButtonInnerFunction(r)
+	})
 }
 
 allowNextRadioButton()
@@ -128,11 +132,16 @@ function calculateCourse() {
 		MECAPoints += r.value * data[i].MECAPoints
 		EDFPoints += r.value * data[i].EDFPoints
 
-		totalADM += 5 * data[i].ADMPoints
-		totalDS += 5 * data[i].DSPoints
-		totalMECA += 5 * data[i].MECAPoints
-		totalEDF += 5 * data[i].EDFPoints
+		totalADM += Math.pow(data[i].ADMPoints, 2)
+		totalDS += Math.pow(data[i].DSPoints, 2)
+		totalMECA += Math.pow(data[i].MECAPoints, 2)
+		totalEDF += Math.pow(data[i].EDFPoints, 2)
 	})
+
+	totalADM *= 1.15
+	totalDS *= 1.15
+	totalMECA *= 1.15
+	totalEDF *= 1.15
 
 
 	const ADMPercentage = (parseFloat(ADMPoints/totalADM) * 100).toFixed(1)
