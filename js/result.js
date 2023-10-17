174f0218354bb62eafee94f8840943085ad47f50
Arthur Santana <arthur.alexandre330@gmail.com>
2023-10-17 14:30:29 -0300

    f

--- js/result.js
+++ js/result.js
@@ -10,19 +10,19 @@ const courseImageElement = document.querySelector('img.main__img__img')
 const courseDescriptionElement = document.querySelector('p.course-description')
 
 const coursesInfo = [
-    {
-        course: "Administração",
-        content: `A Administração é uma área que exige habilidades diversas, como <strong>organização, tomada de decisões, habilidades interpessoais e visão estratégica</strong>. Sua capacidade de <strong>lidar com desafios, sua proatividade e sua habilidade para trabalhar bem em equipe</strong> certamente serão ativos valiosos ao longo do curso e em sua futura carreira na área.<br><br>Ao escolher Administração, você está se preparando para <strong>enfrentar os desafios dinâmicos do mundo dos negócios e contribuir para o sucesso de organizações</strong>. A Etec é um ambiente propício para o desenvolvimento de suas habilidades e conhecimentos, e estou confiante de que você aproveitará ao máximo essa oportunidade.`
-    }, {
-        course: "Desenvolvimento de Sistemas",
-        content: `O Desenvolvimento de Sistemas é uma área fascinante e dinâmica, e estou certo de que suas habilidades e características pessoais se alinham perfeitamente com as demandas desse campo. A capacidade de <strong>resolver problemas, a criatividade na criação de soluções, a paciência para depurar códigos complexos e a habilidade de trabalhar em equipe</strong> são aspectos fundamentais para o sucesso nesse curso, e estou confiante de que você os possui.<br><br>Além disso, a <strong>dedicação e o comprometimento</strong> que você demonstrou ao realizar o teste são indicativos claros de que você está disposto a enfrentar desafios e a se aprimorar constantemente. O Desenvolvimento de Sistemas é uma área que exige aprendizado contínuo, e sua atitude positiva em relação aos desafios será uma grande vantagem ao longo de sua jornada acadêmica e profissional.`
-    }, {
-        course: "Edificações",
-        content: `Sua escolha por Edificações revela não apenas um interesse em <strong>construção e design</strong>, mas também uma habilidade inata para compreender e trabalhar com estruturas. Este campo exige precisão, criatividade e uma mente analítica, e estou confiante de que você possui todas essas qualidades.<br><br>A área de Edificações é essencial para o desenvolvimento e aprimoramento das cidades, contribuindo para a construção de infraestruturas sólidas e ambientes funcionais. Ao escolher esse curso, você está se posicionando para desempenhar um <strong>papel crucial na sociedade</strong>, moldando o ambiente ao nosso redor.`
-    }, {
-        course: "Mecatrônica",
-        content: `Sua afinidade com Mecatrônica destaca sua habilidade em lidar com desafios que exigem uma abordagem multidisciplinar. Este curso combina elementos da <strong>engenharia mecânica, eletrônica e de controle</strong>, e tenho certeza de que sua capacidade de pensar de forma holística será uma vantagem valiosa.<br><br>Além disso, Mecatrônica exige <strong>criatividade e inovação</strong>, e estou certo de que você trará ideias frescas para resolver problemas complexos. Sua dedicação aos estudos e o interesse demonstrado na interseção entre mecânica e eletrônica são qualidades que certamente o destacarão no curso.`
-    }
+	{
+		course: "Administração",
+		content: `A Administração é uma área que exige habilidades diversas, como <strong>organização, tomada de decisões, habilidades interpessoais e visão estratégica</strong>. Sua capacidade de <strong>lidar com desafios, sua proatividade e sua habilidade para trabalhar bem em equipe</strong> certamente serão ativos valiosos ao longo do curso e em sua futura carreira na área.<br><br>Ao escolher Administração, você está se preparando para <strong>enfrentar os desafios dinâmicos do mundo dos negócios e contribuir para o sucesso de organizações</strong>. A Etec é um ambiente propício para o desenvolvimento de suas habilidades e conhecimentos, e estou confiante de que você aproveitará ao máximo essa oportunidade.`
+	}, {
+		course: "Desenvolvimento de Sistemas",
+		content: `O Desenvolvimento de Sistemas é uma área fascinante e dinâmica, e estou certo de que suas habilidades e características pessoais se alinham perfeitamente com as demandas desse campo. A capacidade de <strong>resolver problemas, a criatividade na criação de soluções, a paciência para depurar códigos complexos e a habilidade de trabalhar em equipe</strong> são aspectos fundamentais para o sucesso nesse curso, e estou confiante de que você os possui.<br><br>Além disso, a <strong>dedicação e o comprometimento</strong> que você demonstrou ao realizar o teste são indicativos claros de que você está disposto a enfrentar desafios e a se aprimorar constantemente. O Desenvolvimento de Sistemas é uma área que exige aprendizado contínuo, e sua atitude positiva em relação aos desafios será uma grande vantagem ao longo de sua jornada acadêmica e profissional.`
+	}, {
+		course: "Edificações",
+		content: `Sua escolha por Edificações revela não apenas um interesse em <strong>construção e design</strong>, mas também uma habilidade inata para compreender e trabalhar com estruturas. Este campo exige precisão, criatividade e uma mente analítica, e estou confiante de que você possui todas essas qualidades.<br><br>A área de Edificações é essencial para o desenvolvimento e aprimoramento das cidades, contribuindo para a construção de infraestruturas sólidas e ambientes funcionais. Ao escolher esse curso, você está se posicionando para desempenhar um <strong>papel crucial na sociedade</strong>, moldando o ambiente ao nosso redor.`
+	}, {
+		course: "Mecatrônica",
+		content: `Sua afinidade com Mecatrônica destaca sua habilidade em lidar com desafios que exigem uma abordagem multidisciplinar. Este curso combina elementos da <strong>engenharia mecânica, eletrônica e de controle</strong>, e tenho certeza de que sua capacidade de pensar de forma holística será uma vantagem valiosa.<br><br>Além disso, Mecatrônica exige <strong>criatividade e inovação</strong>, e estou certo de que você trará ideias frescas para resolver problemas complexos. Sua dedicação aos estudos e o interesse demonstrado na interseção entre mecânica e eletrônica são qualidades que certamente o destacarão no curso.`
+	}
 ]
 
 const coursesAffinity = [
@@ -49,7 +49,7 @@ const coursesAffinity = [
 
 ]
 
-coursesAffinity.sort((a,b) => {
+coursesAffinity.sort((a, b) => {
 	if (b.affinity - a.affinity != 0) return b.affinity - a.affinity
 
 	return Math.random() - 0.5
@@ -97,7 +97,7 @@ courseImageElement.src = `./assets/${idealCourse}.png`
 console.log(courseImageElement);
 courseDescriptionElement.innerHTML = coursesInfo[coursesInfo.indexOf(idealFullCourse)]
 
-coursesInfo.forEach(c => {if(c.course == idealFullCourse) courseDescriptionElement.innerHTML = c.content})
+coursesInfo.forEach(c => { if (c.course == idealFullCourse) courseDescriptionElement.innerHTML = c.content })
 
 
 
